8ec646a2fd8f40abd2f66fd96bf9f1ce074abbcc
ghit <claudiu.ghit96@e-uvt.ro>
2023-02-06 02:43:16 +0200

    Springboot showroom first commit

---
+++ src/main/java/com/showroom/springboot/SpringbootShowroomApplication.java
@@ -0,0 +1,13 @@
+package com.showroom.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShowroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShowroomApplication.class, args);
+	}
+
+}
